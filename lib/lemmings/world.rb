25e04fda85cd1f8da6aa333aab3b391eec039b51
Josh Adams <josh@isotope11.com>
2013-02-24 19:19:59 -0600

    Add some position value object goodness

--- lib/lemmings/world.rb
+++ lib/lemmings/world.rb
@@ -19,42 +19,66 @@ module Lemmings
       @rows.first.count
     end
 
-    def add_object(object, x, y)
-      @rows[y][x] << object
+    def add_object(object, position)
+      cell_at_position(position) << object
     end
 
-    def objects_at(x, y)
-      @rows[y][x]
+    def objects_at(position)
+      cell_at_position(position)
     end
 
     def position_for(object)
       # LOL SO INEFFICIENT
       positions.detect do |p|
-        objects_at(*p).include?(object)
+        objects_at(p).include?(object)
       end
     end
 
     def move_right(object)
-      current_position = position_for(object)
-      objects_at(*current_position).delete(object)
-      add_object(object, current_position[0] + 1, current_position[1])
+      move_to_the(:east, object)
     end
 
     def move_left(object)
-      current_position = position_for(object)
-      objects_at(*current_position).delete(object)
-      add_object(object, current_position[0] - 1, current_position[1])
+      move_to_the(:west, object)
     end
 
     private
+    def move_to_the(position_term, object)
+      current_position = position_for(object)
+      objects_at(current_position).delete(object)
+      add_object(object, current_position.public_send(position_term))
+    end
+
+    def cell_at_position(position)
+      @rows[position.y][position.x]
+    end
+
     def positions
       pos = []
       (0..height-1).each do |y|
         (0..width-1).each do |x|
-          pos << [x, y]
+          pos << Position.new(x, y)
         end
       end
       pos
+    end
+
+    class Position < Struct.new(:x, :y)
+      def north
+        Position.new(x=self.x, y=self.y-1)
+      end
+
+      def south
+        Position.new(x=self.x, y=self.y+1)
+      end
+
+      def east
+        Position.new(x=self.x+1, y=self.y)
+      end
+
+      def west
+        Position.new(x=self.x-1, y=self.y)
+      end
     end
   end
 end
--- spec/unit/world_spec.rb
+++ spec/unit/world_spec.rb
@@ -14,29 +14,27 @@ describe World do
 
   it "can have an object added at specified coordinates" do
     mock_object = mock
-    x = 10
-    y = 12
-    subject.add_object(mock_object, x, y)
-    subject.objects_at(x, y).must_equal [mock_object]
+    position = World::Position.new(x=10, y=12)
+    subject.add_object(mock_object, position)
+    subject.objects_at(position).must_equal [mock_object]
   end
 
   describe "object positions" do
     let(:mock_object){ mock }
-    let(:x){ 10 }
-    let(:y){ 20 }
+    let(:position){ World::Position.new(x=10, y=20) }
 
     before do
-      subject.add_object(mock_object, x, y)
+      subject.add_object(mock_object, position)
     end
 
     it "knows where an object is" do
-      subject.position_for(mock_object).must_equal([x, y])
+      subject.position_for(mock_object).must_equal(position)
     end
 
     it "knows how to move an object to the right" do
       subject.move_right(mock_object)
-      subject.objects_at(x, y).must_equal([])
-      subject.objects_at(x+1, y).must_equal([mock_object])
+      subject.objects_at(position).must_equal([])
+      subject.objects_at(position.east).must_equal([mock_object])
     end
   end
 end
